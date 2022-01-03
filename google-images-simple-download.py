a7aac8b488ad2faaa08f0536162b6a917c7c4309
Dário Estevão <darioems@gmail.com>
2022-01-03 19:40:30 -0300

    Create google-images-simple-download.py

---
+++ google-images-simple-download.py
@@ -0,0 +1,22 @@
+from custom_simple_image_download import simple_image_download as simp 
+import csv
+OpenFileCSV = 'Produtos-carajas-to-CVS-Magento - Top-6210-Magento-import.csv'
+openProducts = open(OpenFileCSV, encoding='utf-8', errors='ignore')
+productsCarajas = csv.reader(openProducts, delimiter= ",")
+productsCarajasData = list(productsCarajas)
+
+myDict = dict()
+
+for row in productsCarajasData:
+    myDict[row[0]] = row[7]
+
+# fatiar a lista de produtos
+#sliceDict = dict(list(myDict.items())[4960:6210])
+sliceDict = dict(list(myDict.items()))
+
+response = simp.simple_image_download
+
+for k, v in sliceDict.items():
+    print(k,v)
+    response().download(v.replace("/", ' '), 2, k)
+
